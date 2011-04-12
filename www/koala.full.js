1e9379fbb19474d545a9582d91482d1b54df9462
Colin Kuebler <kueblc@rpi.edu>
2011-04-12 14:39:33 -0400

    Added main js Separated CSS into core (structure) and the default theme

---
+++ www/koala.full.js
@@ -0,0 +1,14 @@
+// koala.full.js
+// part of the koala project
+
+var koala = {};
+
+window.onload = function(){
+	// TODO
+	// testing...
+	koala.editor = document.getElementById("code");
+	// trigger designmode
+	koala.editor.designMode = 'on';
+	koala.editor.contentEditable = true;
+	
+}
