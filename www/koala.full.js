b2ef78dfc724c9f495d8893f64acb3c1992883f6
Colin Kuebler <kueblc@rpi.edu>
2011-04-13 13:25:30 -0400

    Added a theme, updated koalabook

--- www/koala.full.js
+++ www/koala.full.js
@@ -14,19 +14,23 @@ window.onload = function(){
 	koala.editor.contentEditable = true;
 	
 	koala.theme = $("theme");
-	for( var i = 2; i < document.styleSheets.length; i++ ){
+	for( var i = 1; i < document.styleSheets.length; i++ ){
 		document.styleSheets[i].disabled = true;
 	}
-	koala.theme.current = document.styleSheets[1];
+	
+	koala.theme.current = document.styleSheets[2];
+	koala.theme.current.disabled = false;
+	koala.theme.options[1].selected = "selected";
+	
 	koala.theme.onchange = function(){
 		koala.theme.current.disabled = true;
 		koala.theme.current = document.styleSheets[ this.selectedIndex+1 ]
 		koala.theme.current.disabled = false;
-		/* TODO
+		// TODO
 		// the following method doesn't seem to work in webkit
 		// for now, all the stylesheets are loaded and then disabled
 		// find a better way to load stylesheets dynamically
-		koala.theme.current.disabled = true;
+		/*koala.theme.current.disabled = true;
 		for( var i = 1; i < document.styleSheets.length; i++ ){
 			if( document.styleSheets[i].title == this.value ){
 				document.styleSheets[i].disabled = false;
@@ -35,12 +39,14 @@ window.onload = function(){
 			}
 		}
 		var e = document.createElement("link");
-		e.type = "text/css";
-		e.rel = "stylesheet";
-		e.href = "themes/" + this.value + ".css";
-		e.title = this.value;
+		e.setAttribute("type","text/css");
+		e.setAttribute("rel","stylesheet");
+		e.setAttribute("href","themes/" + this.value + ".css");
+		e.setAttribute("title",this.value);
 		document.getElementsByTagName("head")[0].appendChild(e);
-		koala.theme.current = document.styleSheets[ document.styleSheets.length-1 ];
+		//.innerHTML+="<link type='text/css' rel='stylesheet' href='themes/"+this.value+".css' title='"+this.value+"'>";
+		koala.theme.current = e.sheet;
+		//koala.theme.current = document.styleSheets[ document.styleSheets.length-1 ];
 		koala.theme.current.disabled = false;*/
 	}
 };
