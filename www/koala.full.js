05eb47c3cc3eff63c01b1a634c462ee1913b49cb
Colin Kuebler <kueblc@rpi.edu>
2011-04-13 00:57:09 -0400

    Added theme switching panel and two themes

--- www/koala.full.js
+++ www/koala.full.js
@@ -1,14 +1,46 @@
 // koala.full.js
-// part of the koala project
+// the koala project
 
 var koala = {};
+
+function $(e){ return document.getElementById(e); };
 
 window.onload = function(){
 	// TODO
 	// testing...
-	koala.editor = document.getElementById("code");
+	koala.editor = $("code");
 	// trigger designmode
 	koala.editor.designMode = 'on';
 	koala.editor.contentEditable = true;
 	
-}
+	koala.theme = $("theme");
+	for( var i = 2; i < document.styleSheets.length; i++ ){
+		document.styleSheets[i].disabled = true;
+	}
+	koala.theme.current = document.styleSheets[1];
+	koala.theme.onchange = function(){
+		koala.theme.current.disabled = true;
+		koala.theme.current = document.styleSheets[ this.selectedIndex+1 ]
+		koala.theme.current.disabled = false;
+		/* TODO
+		// the following method doesn't seem to work in webkit
+		// for now, all the stylesheets are loaded and then disabled
+		// find a better way to load stylesheets dynamically
+		koala.theme.current.disabled = true;
+		for( var i = 1; i < document.styleSheets.length; i++ ){
+			if( document.styleSheets[i].title == this.value ){
+				document.styleSheets[i].disabled = false;
+				koala.theme.current = document.styleSheets[i];
+				return;
+			}
+		}
+		var e = document.createElement("link");
+		e.type = "text/css";
+		e.rel = "stylesheet";
+		e.href = "themes/" + this.value + ".css";
+		e.title = this.value;
+		document.getElementsByTagName("head")[0].appendChild(e);
+		koala.theme.current = document.styleSheets[ document.styleSheets.length-1 ];
+		koala.theme.current.disabled = false;*/
+	}
+};
