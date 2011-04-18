81ed58016eec69f095ee383a4f4e95fc01b68de4
Colin Kuebler <kueblc@rpi.edu>
2011-04-18 16:04:44 -0400

    Working on editor and highlighting Experimenting with using <pre> instead of <div> Improved compatibility with IE using try/catch

--- www/koala.full.js
+++ www/koala.full.js
@@ -5,6 +5,23 @@ var koala = {};
 
 function $(e){ return document.getElementById(e); };
 
+// testing...
+function hl(){
+	//var marker = $("cur");
+	//if( marker ) koala.editor.removeChild(marker);
+	koala.editor.innerHTML = koala.editor.innerHTML
+	.replace( /(<br>|<div>|<\/div><div>|<\/P>)/mg, "\n" )
+	.replace( /<.*?>/mg, "" )
+	.replace( /\b(say|put|in)\b/mg, "<span class='command'>$1</span>" )
+	.replace( /\b(\d+)\b/mg, "<span class='number'>$1</span>" )
+	.replace( /\[(.*?)\]/mg, "<span class='string'>[$1]</span>" )
+	.replace( /(\/\/.*?)\n/mg, "<span class='comment'>$1</span><br>" )
+//	.replace( /\n/mg, "<br>" )
+	.replace( /\x1f/m, "<span id='cur'><span>" )
+	//.replace( /(\*\/.*?\*\/)/mg, "<span class='error'>$1</span>" )
+	//.replace( /(\b\w+?\b)/mg, "<span class='word'>$1</span>" )
+;};
+
 window.onload = function(){
 	// TODO
 	// testing...
@@ -54,7 +71,19 @@ window.onload = function(){
 	$("btn_text").onclick = function(){
 		alert(koala.editor.textContent || koala.editor.innerText); };
 	$("btn_html").onclick = function(){ alert(koala.editor.innerHTML); };
-	$("btn_hl").onclick = hl();
+	$("btn_hl").onclick = function(){hl();};
+	$("btn_run").onclick = function(){
+		// for testing...
+		hl();
+		var lex = koala.editor.getElementsByTagName("span");
+		for( var i = 0; i < lex.length; i++ ){
+			if( lex[i].innerHTML === "say" ){
+				i++;
+				var str = lex[i].textContent || lex[i].innerText;
+				alert( str.slice(1,-1) );
+			}
+		}
+	};
 	koala.editor.onkeydown = function(e){
 		e = e || window.event;
 		var k = e.keyCode || e.which;
@@ -89,8 +118,10 @@ window.onload = function(){
 			//koala.editor.focus();
 		}
 		if( k === 9 ){ /* tab */
-			document.execCommand("inserthtml",false,"&nbsp;&nbsp;&nbsp;&nbsp;");
-			return false;
+			try {
+				document.execCommand("inserthtml",false,"    ");
+				return false;
+			} catch(e){}
 		}
 	};
 	koala.editor.onkeyup = function(e){
@@ -120,29 +151,16 @@ window.onload = function(){
 				o = o-r.textContent.length+1;
 				s.collapse(koala.editor,o);
 			}*/
-			/*window.document.execCommand("inserthtml",false,"~");
-			hl();
-			window.getSelection().collapse($("cur"),0);*/
+			try {
+				document.execCommand("inserthtml",false,"\x1f");
+				hl();
+				window.getSelection().collapse($("cur"),0);
+			} catch(e) {
+				hl();
+			}
 		}
 	}
 	koala.editor.focus();
 	//window.document.execCommand("inserthtml",false,"<span></span>");
 	//window.document.execCommand("bold",false,null);
 };
-
-// testing...
-function hl(){
-		var marker = $("cur");
-		if( marker ) koala.editor.removeChild(marker);
-		koala.editor.innerHTML = koala.editor.innerHTML
-		.replace( /(<br>|<\/div>)/mg, "\n" )
-		.replace( /<.*?>/mg, "" )
-		.replace( /\b(say|yell)\b/mg, "<span class='command'>$1</span>" )
-		.replace( /\b(\d+)\b/mg, "<span class='number'>$1</span>" )
-		.replace( /\[(.*?)\]/mg, "<span class='string'>[$1]</span>" )
-		.replace( /(\/\/.*?)\n/mg, "<span class='comment'>$1</span><br>" )
-		.replace( /\n/mg, "<br>" )
-		//.replace( /~/mg, "<span id='cur'><span>" )
-		//.replace( /(\*\/.*?\*\/)/mg, "<span class='error'>$1</span>" )
-		//.replace( /(\b\w+?\b)/mg, "<span class='word'>$1</span>" )
-	;};
