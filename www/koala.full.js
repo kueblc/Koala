84a072b3dfaf305dcc1f376cf0db4583590b7a8b
Colin Kuebler <kueblc@rpi.edu>
2012-01-19 19:02:45 -0500

    Rewrote algorithm to account for edits in multiple points in the document

--- www/koala.full.js
+++ www/koala.full.js
@@ -35,6 +35,7 @@ koala = {
 			koala.lang.parser = new RegExp( rulesrc.join('|'), "gi" );
 		},
 		parse: function(){
+			console.log("Running koala.lang.parse");
 			if( !koala.lang.parser ) koala.lang.genparser();
 			var input = koala.editor.input.value,
 				output = '';
@@ -46,20 +47,29 @@ koala = {
 				var m = input.match(koala.lang.parser);
 				var n = parent.childNodes;
 				//var n = parent.getElementsByTagName("span");
-				var i, j;
-				// find the starting point of differences
+				var i, j, mp, dp;
+				// find the first difference
 				for( i = 0; i < m.length && i < n.length-1; i++ )
 					if( m[i] !== n[i].textContent ) break;
+				// find the last difference
+				for( mp = m.length-1, np = n.length-2; i < mp && i < np; mp--, np-- )
+					if( m[mp] !== n[np].textContent ) break;
+				// remove dirty spans
+				for( j = np - i + 1; j > 0; j-- ){
+					console.log("Removing span");
+					parent.removeChild(n[i]);
+				}
 				// if the length of the display is longer than the parse, delete excess display
-				while( m.length < n.length-1 ) parent.removeChild(n[i]);
+				/*while( m.length < n.length-1 ) parent.removeChild(n[i]);
 				// find the ending point of differences
 				for( j = i + m.length - n.length+1; j < m.length; j++ ){
 					if( m[j] === n[i].textContent ) break;
 					parent.removeChild(n[i]);
-				}
+				}*/
 				// add in modified spans
-				console.log( (j-i) + " modified spans.");
-				for( var insertionPt = n[i]; i < j; i++ ){
+				//console.log( (j-i) + " modified spans.");
+				for( var insertionPt = n[i]; i <= mp; i++ ){
+					console.log("Adding span");
 					span = document.createElement("span");
 					span.className = assoc(m[i]);
 					span.textContent = span.innerText = m[i];
