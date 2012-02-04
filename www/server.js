856d315e175f495e08056b0259a9e4368cd788c3
Colin Kuebler <kueblc@rpi.edu>
2012-02-04 17:47:21 -0500

    Added some request info

--- www/server.js
+++ www/server.js
@@ -1,18 +1,19 @@
 // server.js
 // the koala project
 
-var PORT = 8124;
+var HOST = '127.0.0.1',
+	PORT = 8124;
 
 var http = require('http');
 
 var server = http.createServer(
 	function( request, response ){
-		console.log('Handling request...');
+		console.log('Handling '+request.method+' request http://'+HOST+':'+PORT+request.url);
 		response.writeHead( 200, {'Content-Type': 'text/plain'} );
 		response.end( 'Hello World!\n' );
 	});
 
 server.listen(PORT);
 
-console.log('Listening on http://127.0.0.1:'+PORT+'/...');
+console.log('Listening on http://'+HOST+':'+PORT+'/...');
 
