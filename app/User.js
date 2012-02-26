bddd884247edf587ba7066f7c5030757657a69cb
Colin Kuebler <kueblc@rpi.edu>
2012-02-26 15:48:21 -0500

    Developing login system...

--- app/User.js
+++ app/User.js
@@ -5,13 +5,38 @@
  */
 
 function User(){
+	var api = this;
 	
-	theme = $("theme");
+	var theme = $("theme");
 	theme.selector = $("theme_sel");
 	
 	theme.selector.onchange = function(){
 		theme.href = "themes/" + this.value + ".css";
 	};
 	
+	var user_field = $("login_username"),
+		pass_field = $("login_password");
+	
+	var li = user_field.parentNode.parentNode.parentNode;
+	
+	var setLi = function( str ){
+		li.replaceChild( document.createTextNode(str), li.firstChild );
+	};
+	
+	api.login = function( user, pass ){
+		setLi("logged in as "+user);
+	};
+	
+	pass_field.onkeydown = function(e){
+		var e = e || window.event;
+		var key = e.which || e.keyCode || e.charCode;
+		if( key === 13 ){
+			api.login( user_field.value, pass_field.value );
+			return false;
+		}
+		return true;
+	};
+	
+	return api;
 };
 
