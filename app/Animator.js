d415becd9b0149e1ccade383957a1086aa545d47
Colin Kuebler <kueblc@rpi.edu>
2012-07-11 13:51:37 -0400

    Working on new Animation class, a JS fallback for CSS3 transitions

--- app/Animator.js
+++ app/Animator.js
@@ -4,6 +4,28 @@
  * Handles all animation effects
  */
 
+function Animation( element, properties, units, duration, rate, transition, cb ){
+	// record the start time
+	var startTime = new Date();
+	
+	// obtain starting and differential values
+	var styles = element.style,
+		start = {},
+		diff = {};
+	for( prop in properties ){
+		// regex to extract numbers
+		start[prop] = Number(/-?\d+(\.\d+)?/.exec(styles[prop]));
+		diff[prop] = properties[prop] - start[prop];
+	}
+	
+	// animation timer
+	var timer = window.setInterval( function(){
+		var time = new Date() - startTime;
+		var progress = time / duration;
+		
+	}, rate );
+};
+
 function Animator(){
 	/* INIT */
 	var api = this;
