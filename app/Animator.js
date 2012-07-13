270b455c3601db8c10f9257cba53fcec2b724bca
Colin Kuebler <kueblc@rpi.edu>
2012-07-13 01:21:30 -0400

    Bugfix in determining the end of an animation

--- app/Animator.js
+++ app/Animator.js
@@ -32,11 +32,11 @@ function Animation( element, properties, units, duration, rate, transition, cb )
 	var timer = window.setInterval( function(){
 		// determine the animation progress
 		var time = new Date() - startTime;
-		var progress = Math.min( transition( time / duration ), 1 );
+		var progress = transition( Math.min( time / duration, 1 ) );
 		// set property values
 		for( prop in properties )
 			style[prop] = (diff[prop] * progress + start[prop]) + units[prop];
-		if( progress === 1 ){
+		if( time >= duration ){
 			// clear timer and callback
 			window.clearInterval(timer);
 			cb && cb();
