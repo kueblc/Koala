31ba0f53ffb81454e5c84e329cefd68d1f6e9188
Colin Kuebler <kueblc@rpi.edu>
2012-10-10 22:33:42 -0400

    Cleaned up error reporting

--- app/Utils.js
+++ app/Utils.js
@@ -45,21 +45,34 @@ String.prototype.contains = function(x){
 	return this.indexOf(x) !== -1;
 };
 
-window.onerror = function( msg, url, line ){
-	function toHex(s){
-		var output = "";
-		var b16 = "0123456789ABCDEF";
-		for( var i = 0; i < s.length; i++ ){
-			var c = s.charCodeAt(i);
-			output += b16.charAt(c>>4) + b16.charAt(c&15) + " ";
-		}
-		return output;
+String.prototype.suffix = function(delim){
+	return this.substring( this.lastIndexOf(delim) + 1 );
+};
+
+function toHex(s){
+	var output = "";
+	var b16 = "0123456789ABCDEF";
+	for( var i = 0; i < s.length; i++ ){
+		var c = s.charCodeAt(i);
+		output += b16.charAt(c>>4) + b16.charAt(c&15) + " ";
 	}
+	return output;
+};
+
+window.onerror = function( msg, url, line ){
+	// construct an error report
+	var report = {
+		Location: url.suffix('/') + ':' + line,
+		Message: msg,
+		UserAgent: navigator.userAgent };
+	// create a text representation of the report
+	var reportStr = ''
+	for( var i in report ) reportStr += i + ': ' + report[i] + '\n';
+	// display the error report, ala BSOD easter egg
 	document.body.style.background = "blue";
-	var error = url.substring( url.lastIndexOf('/')+1 ) + 
-		":" + line + ": " + msg + "\n" + navigator.userAgent;
 	document.body.innerHTML =
-		"<div id='bsod'><h1>the koala project</h1><p>A fatal error has occurred. An error report is being filed. Report details:</p><pre>"+error+"</pre><p>Press any key to continue</p></div>";
+		"<div id='bsod'><h1>the koala project</h1><p>A fatal error has occurred. An error report is being filed. Report details:</p><pre>" +
+		reportStr + "</pre><p>Press any key to continue</p></div>";
 	document.onkeypress = function(){ location.reload(); };
 	return true;
 };
